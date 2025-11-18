3d9f7d5cb9457325a9a9b360ecba39497fe0328f
PRIYANSHU <priyank2657@gmail.com>
2025-11-18 23:46:58 +0530

    new react folder updated

---
+++ react-bit2/src/Components/ProfileCard.jsx
@@ -0,0 +1,14 @@
+import ProfileCard from "./ProfileCard";
+
+<ProfileCard
+  name="Javi A. Torres"
+  title="Software Engineer"
+  handle="javicodes"
+  status="Online"
+  contactText="Contact Me"
+  avatarUrl="/path/to/avatar.jpg"
+  showUserInfo={true}
+  enableTilt={true}
+  enableMobileTilt={false}
+  onContactClick={() => console.log("Contact clicked")}
+/>;
